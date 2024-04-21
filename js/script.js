40f198c0b9516d42b254dcbe164e5966311fb0fd
lucbac06 <lucas.backdahl@gmail.com>
2024-04-21 20:54:46 +0200

    products

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 document.querySelector(".menu-button").addEventListener("click", showMenu);
 
-document.querySelector(".fade-layer").addEventListener("click", showMenu);
-
-
-function showMenu(){
-    // let menu = document.querySelector(".menu");
-    // menu.classList.toggle("show");
-
+function showMenu() {
     let menu = document.querySelector(".menu-button");
     menu.classList.toggle("show-menu");
 
-    let layer = document.querySelector(".fade-layer");
-    layer.classList.toggle("visible");
+    let menuItems = document.querySelector(".menuItems");
+    menuItems.classList.toggle("visible");
 }
